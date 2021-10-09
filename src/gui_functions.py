8dd83c0e95aba4c9063ec362e0fc013622694edc
Kareem Skinner <kareem@karsteski.com>
2021-10-09 19:30:59 -0400

    Finished 'New Word' button

--- src/gui_functions.py
+++ src/gui_functions.py
@@ -9,7 +9,6 @@ class GameGUI(object):
 
     LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    buttons = []
     current_word = []
 
     def __init__(self, window_width, window_height):
@@ -18,8 +17,11 @@ class GameGUI(object):
 
         self.viewport = dpg.create_viewport(title="Hackathon Game - Boggle", width=window_width, height=window_height)
 
-    def button_callback(self, sender, data):
+    def grid_button_callback(self, sender, data):
         self.current_word.append(dpg.get_item_label(sender))
+
+    def new_word_callback(self, sender, data):
+        self.current_word.clear()
 
     def start(self):
         dpg.setup_dearpygui(viewport=self.viewport)
@@ -29,7 +31,7 @@ class GameGUI(object):
             for x in range(5):
                 for y in range(5):
                     dpg.add_button(
-                        label=random.choice(self.LETTERS), id=(str(x) + str(y)), callback=self.button_callback
+                        label=random.choice(self.LETTERS), id=(str(x) + str(y)), callback=self.grid_button_callback
                     )
                     if y < 4:
                         dpg.add_same_line()
@@ -37,7 +39,7 @@ class GameGUI(object):
             # Game Function Buttons
             dpg.add_button(id="reset", label="Reset")
             dpg.add_same_line()
-            dpg.add_button(id="new_word", label="New Word")
+            dpg.add_button(id="new_word", label="New Word", callback=self.new_word_callback)
             dpg.add_same_line()
             dpg.add_button(id="set_word", label="Set Word")
 
