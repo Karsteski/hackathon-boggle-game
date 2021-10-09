9a58065854b18269bfa3b63f2c6d18a732305ca4
Kareem Skinner <kareem@karsteski.com>
2021-10-09 13:47:57 -0400

    formatted code

--- src/boggle-game.py
+++ src/boggle-game.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 
-print("Hello World")
-
 import random
-LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-N=5
+
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+N = 5
+
+
 class Boggle:
-    def __init__(self,pname1,pname2):
+    def __init__(self, pname1, pname2):
         self.pname1 = pname1
         self.pname2 = pname2
-        
-        self.g=[
-               ['-']*N for i in range(N)
-               ]
 
-        
+        self.g = [["-"] * N for i in range(N)]
+
     def scramble_board(self):
         for j in range(N):
             for i in range(N):
                 self.g[j][i] = random.choice(LETTERS)
+
     def stringifyg(self):
-        rv=''
+        rv = ""
         for row in self.g:
             for cell in row:
-                rv+=str(cell)
-            rv+='\n'
-        rv+='\n'
+                rv += str(cell)
+            rv += "\n"
+        rv += "\n"
         return rv
+
     def print(self):
         print(self.stringifyg())
 
-boggle=Boggle("ONE","TWO")
+
+boggle = Boggle("ONE", "TWO")
 boggle.scramble_board()
 boggle.print()
