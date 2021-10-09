c305a2c8616996a571b92d99eae46130cfe9e6e9
AndrewWigginCout <x@aol.com>
2021-10-09 10:24:34 -0700

    def scramble

--- src/boggle-game.py
+++ src/boggle-game.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 
 print("Hello World")
+
+import random
+letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+N=5
+x=[
+   ['s']*N for i in range(N)
+   ]
+
+def scramble_board(g):
+    for j in range(N):
+        for i in range(N):
+            g[j][i] = random.choice(letters)
+def stringifyg(g):
+    rv=''
+    for row in g:
+        for cell in row:
+            rv+=str(cell)
+        rv+='\n'
+    rv+='\n'
+    return rv
+
+scramble_board(x)
+print(stringifyg(g))
+print(x)
