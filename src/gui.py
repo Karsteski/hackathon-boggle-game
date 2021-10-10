b16b03649bfab68c17e643d18718fee506fa7e9c
Ved9rakash <vs19990711@gmail.com>
2021-10-10 19:13:57 +0530

    Boggle Game

--- src/gui.py
+++ src/gui.py
@@ -5,7 +5,7 @@ from boggle_game import *
 # Test Grid
 test_grid = [["a", "f"], ["b", "g"], ["c", "h"], ["d", "i"], ["e", "j"]]
 
-gui = GameGUI(150, 150, test_grid)
+gui = GameGUI(800, 600, test_grid)
 gui.start()
 
 while dpg.is_dearpygui_running():
